e500b7bf34c2f93901c857b11aafbf7f376f3165
Forsevin <kvnforsell@outlook.com>
2014-04-08 19:19:45 +0200

    Added example

---
+++ examples/movement.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/Forsevin/n3/n3"
+
+func main() {
+	engine := n3.New()
+
+	// Load our resources
+	engine.Assets().LoadImageAsset("player.bmp")
+	engine.Assets().LoadScriptAsset("player.js")
+
+	// Create player
+	player := engine.CreateObject()
+	sprite := player.AddComponent(n3.NewSpriteComponent()).(*n3.SpriteComponent)
+	script := player.AddComponent(n3.NewScriptComponent()).(*n3.ScriptComponent)
+
+	// Set script and sprite
+	script.Src = engine.Assets().ScriptAsset("player.js")
+	sprite.Texture.Texture = engine.Assets().ImageAsset("player.bmp")
+
+	// Add player to scene
+	engine.ActiveScene().AddObject(player)
+
+	engine.Loop()
+}
